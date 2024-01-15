48c9f9bfa59315d828bbf119fbb92c737cd3abbf
nathancree <ncree@unc.edu>
2024-01-15 16:48:16 -0500

    Slight documentation in NumericalMethod.hpp

--- NumericalMethods/RK4Method.hpp
+++ NumericalMethods/RK4Method.hpp
@@ -5,6 +5,11 @@
 #include <string>
 
 class RungeKutta : NumericalMethod {
+// Approximates a solution for an ODE (dy/dx = f(x,y) given initial x and y values. Pick a step size and a target for the solution to
+// y(target). Much better than Euler Method
+// TODO:
+// Can be generalized to the explicit Runge-Kutta method instead of 4th order pick your order and just have as many ks as you need
+// For that could potentially use arrays.
 private:
     double x, y, target, h;
     std::string func;
